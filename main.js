136dd00d1e82e831ad6be074b97580eb3ad62039
Yasmien Gayao <145944404+yjcgayao@users.noreply.github.com>
2023-11-02 06:17:07 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// Existing code in main.js
-
 window.addEventListener('scroll', () => {
   document.querySelector('#Home h1').style.marginTop = `${window.scrollY * 1.5}px`;
   document.querySelector('#Mountain1').style.marginBottom = `${106 - window.scrollY}px`;
@@ -7,22 +5,3 @@ window.addEventListener('scroll', () => {
   document.querySelector('#mainCloud').style.marginTop = `-${window.scrollY}px`;
   document.querySelector('#rightCloud').style.marginRight = `-${window.scrollY}px`;
 });
-
-// Add the typing animation code below
-
-function typeText(element, text, delay) {
-  let index = 0;
-  element.innerHTML = '';
-
-  function type() {
-    if (index < text.length) {
-      element.innerHTML += text.charAt(index);
-      index++;
-      setTimeout(type, delay);
-    }
-  }
-
-  type();
-}
-
-const typingElement = document.getElementById('typing-animation');
